9546ff74ee8a10c06188147d1845e17843f2a666
mnxmnz <minzidev@gmail.com>
2025-06-01 18:36:28 +0900

    docs: React Effect의 생명주기

---
+++ src/07-escape-hatches/05-lifecycle-of-reactive-effects/App.tsx
@@ -0,0 +1,5 @@
+import Pointer from './Pointer';
+
+export default function App() {
+  return <Pointer />;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import MyApp1 from './07-escape-hatches/03-synchronizing-with-effects/App';
 import MyApp2 from './07-escape-hatches/04-you-might-not-need-an-effect/App';
+import MyApp3 from './07-escape-hatches/05-lifecycle-of-reactive-effects/App';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>React Docs</h1>
       <MyApp1 />
       <MyApp2 />
+      <MyApp3 />
     </main>
   );
 }
